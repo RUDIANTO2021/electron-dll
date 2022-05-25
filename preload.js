5bb43b388c62617027c0a6c308c66fee4d6d7ec9
rudianto_fingerspot <rudianto.mysina@gmail.com>
2022-05-25 17:33:09 +0700

    ok

--- preload.js
+++ preload.js
@@ -12,6 +12,8 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 const edge = require('electron-edge-js');
 const path = require('path')
+//const MyReader = edge.func('./dll/FastCodeSDK.dll');
+    
 window.onload = function () {
   const btn = document.getElementById('btn')
   btn.onclick = function () {
@@ -28,23 +30,46 @@ window.onload = function () {
 
   const testDll = document.getElementById('testDll')
   testDll.onclick = function () {
-    alert('Masuk');
-    const Invoke = edge.func({
-      assemblyFile: path.resolve(__dirname, "./dll/FastCodeSDK.dll"),
+    /*const Invoke = edge.func({
+      assemblyFile: path.resolve(__dirname, "./dll/electronedge.dll"),
       typeName: "electronedge.MQ",
       methodName: "Invoke"
+    })*/
+    
+    const Invoke = edge.func({
+      assemblyFile: path.resolve(__dirname, "./dll/FastCodeSDK.dll"),
+      typeName: "FastCodeSDK.FPReader",
+      methodName: "SelectReader"
     })
+
     console.log(Invoke);
-    Invoke('Electron', (error, value) => {
-      //alert('a')
-      if (error) {
-        //console.log(error);
-        throw error; 
-      } else {  
-        console.log(value);
-      }
+    
+    var PARAM = ["5968-3873-0EF2-C868", "D400B01062", "CDC2-A08C-B136-8382-D412-0AD5-9603-B40E", 0];
+    /*
+    Invoke(PARAM, (error, value) => {
+      if (error) throw error;
+      console.log(error, value)
     })
+    */
+    Invoke(PARAM, function (error, result) {
+      if (error) throw error;
+      console.log(result)
+    })
+    
+    //var receiveHandler = Invoke(sendImpl, true); 
+
+    /*var PARAM = ["5968-3873-0EF2-C868", "D400B01062", "CDC2-A08C-B136-8382-D412-0AD5-9603-B40E", 0];
+    //var PARAM = ["SelectReader", "5968-3873-0EF2-C868", "D400B01062", "CDC2-A08C-B136-8382-D412-0AD5-9603-B40E", 0];
+    //SelectReader("5968-3873-0EF2-C868", "D400B01062", "CDC2-A08C-B136-8382-D412-0AD5-9603-B40E", 0)
+
+    Invoke(Invoke.SelectReader("5968-3873-0EF2-C868", "D400B01062", "CDC2-A08C-B136-8382-D412-0AD5-9603-B40E", 0), (error, value) => {
+      if (error) throw error;
+      console.log(error, value)
+    })
+    */
+
+    //var receiveHandler = MyReader("GetReaders"); 
+    //console.log(receiveHandler);
   }
 }
 
-//const ffi = require("@saleae/ffi");
